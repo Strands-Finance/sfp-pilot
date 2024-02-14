fcaaaa11f503e3a706af9facc81d802907bcb416
Henry Wu <hw711@yahoo.com>
2024-02-13 20:18:27 -0400

    clean up unused import

--- contracts/StrandsAPI.sol
+++ contracts/StrandsAPI.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract StrandsAPI is ERC20, AccessControl {
--- contracts/StrandsSFP.sol
+++ contracts/StrandsSFP.sol
@@ -2,10 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import "./synthetix/DecimalMath.sol";
 
 contract StrandsSFP is ERC4626 {
-  using Math for uint256;
 
   constructor(IERC20 depositToken) ERC4626(depositToken) ERC20("Strands Segregated Fund Proxy", "Strands.sfp") {}
 
